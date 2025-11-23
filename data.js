f942d7edd53ddca2faa9d48995ffe28d711b0ac6
Giovanna Testoni Amaral <gicadigital1@gmail.com>
2025-11-23 00:27:21 -0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,26 @@
+dados = [
+{
+"nome": "Allan Kardec", 
+"descricao": "Educador francês e codificador do Espiritismo, sistematizou os ensinamentos dos espíritos nas cinco obras fundamentais, estabelecendo a base racional e moral da doutrina.",
+"obra": "Livro dos Espíritos",
+"link": "https://www.luzespirita.org.br/leitura/pdf/l1.pdf"
+},
+{
+"nome": "Chico Xavier", 
+"descricao": "Referência mundial em psicografia, cedeu os direitos de suas mais de 400 obras mediúnicas à caridades, vivendo a humildade como maior exemplo.",
+"obra": "Entre a terra e o céu",
+"link": "https://tatianabenites.com.br/wp-content/uploads/2020/05/51-entreaterraeoceu.pdf"
+},
+{ 
+"nome": "Divaldo Franco", 
+"descricao": "Médium, psicógrafo prolífico com centenas de livros publicados, educador e um dos maiores oradores espíritas do mundo, fundou a Mansão do Caminho e já realizou milhares de conferências divulgando uma mensagem de paz.",
+"obra": "Entre os dois mundos",
+"link": "https://www.usetupa.com.br/Livros/L-entre-os-dois-mundos.pdf"
+},
+{ 
+"nome": "Zé Arigó", 
+"descricao": "Médium de cura que desafiou a ciência, realizou milhares de cirurgias espirituais e atendimentos gratuitos incorporando o espírito do Dr.Fritz",
+"obra": "Arigó - vida, mediunidade e martírio (por José Herculano Pires)",
+"link": "https://www.luzespirita.org.br/leitura/pdf/L170.pdf"
+}
+];
